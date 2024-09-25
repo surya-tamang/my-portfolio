16478c51cc41d536638ebfdbdbc12245012b01e3
evil-89789 <tngsurya055@gmail.com>
2024-09-25 08:10:08 +0545

    add emailJs

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,70 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import GetInTouch from "../components/GetInTouch";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [processMsg, setProcessMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  const validateForm = (e) => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
     e.preventDefault();
-    const validation =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    setProcessMsg("");
+    setErrorMsg("");
 
-    if (email.match(validation)) {
-      setProcessMsg("Enter a valid email");
+    if (!name || !email || !message) {
+      setErrorMsg("All fields required !!");
+    } else if (name.length < 3) {
+      setErrorMsg("Enter real name !!");
+    } else if (!emailRegex.test(email)) {
+      setErrorMsg("Invalid email !!");
+    } else if (message.length < 5) {
+      setErrorMsg("Leave a message !!");
+    } else {
+      emailjs
+        .sendForm(
+          "service_6v6oz8r",
+          "template_hjb1b6d",
+          form.current,
+          "WUGRDkN04Vd_L-SN_"
+        )
+        .then(
+          () => {
+            setProcessMsg("Message sent successfully!");
+            setEmail("");
+            setName("");
+            setMessage("");
+            setTimeout(() => {
+              setProcessMsg("");
+            }, 1500);
+          },
+          (error) => {
+            setErrorMsg("Failed to send message. Please try again.");
+            console.error("FAILED...", error.text);
+          }
+        );
     }
-
-    // if (name == "" || name.length < 3) {
-    //   setProcessMsg("Please enter a valid name");
-
-    // }
   };
 
   return (
-    <section className="w-11/12 flex items-centeryj justify-center bg-accent md:flex-row flex-col rounded-lg md:py-8 pb-8">
+    <section className="w-11/12 flex items-center justify-center bg-accent md:flex-row flex-col rounded-lg md:py-8 pb-8">
       <div className="md:w-6/12 w-full">
         <GetInTouch />
       </div>
       <div className="w-full md:w-6/12">
         <form
-          // action="https://api.web3forms.com/submit"
-          action=""
-          method="post"
+          ref={form}
           className="flex flex-col gap-5 px-10"
-          onSubmit={validateForm}
+          onSubmit={sendEmail}
         >
-          {/* <input
-            type="hidden"
-            name="590394bb-0bfe-4fd6-9727-9af82518b0ec"
-            value="590394bb-0bfe-4fd6-9727-9af82518b0ec"
-          ></input> */}
           <input
             type="text"
-            name="name"
-            placeholder="Your name"
+            name="user_name"
+            placeholder="Name"
             value={name}
-            onChange={(e) => setName(e.value)}
+            onChange={(e) => {
+              setErrorMsg("");
+              setProcessMsg("");
+              setName(e.target.value);
+            }}
           />
           <input
             type="email"
-            name="email"
-            placeholder="Email address"
-            onChange={(e) => setEmail(e.value)}
+            name="user_email"
+            placeholder="Email"
             value={email}
+            onChange={(e) => {
+              setErrorMsg("");
+              setProcessMsg("");
+              setEmail(e.target.value);
+            }}
           />
           <textarea
             name="message"
-            placeholder="Enter message"
-            onChange={(e) => setMessage(e.value)}
+            placeholder="Message"
             value={message}
+            onChange={(e) => {
+              setErrorMsg("");
+              setProcessMsg("");
+              setMessage(e.target.value);
+            }}
           />
-
-          <span className="h-5 text-center">{processMsg}</span>
-
+          <span className="h-5 text-center text-green">{processMsg}</span>
+          <span className="h-5 text-center text-red-500">{errorMsg}</span>
           <button
             type="submit"
+            value="send"
             className="border-green border py-3 rounded-lg hover:bg-green hover:text-black font-medium"
           >
             Send message
